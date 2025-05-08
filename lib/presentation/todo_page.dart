ae5376240898aa456ed618144c2ddf891eecc5ce
Jantopthegreat <fauzangriya@gmail.com>
2025-05-08 22:36:25 +0700

    membuat bodysafearea dengan  padding

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -7,6 +7,12 @@ class TodoPage extends StatelessWidget {
   Widget build(BuildContext context) {
      final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
-    return const Scaffold();
+    return const Scaffold(
+        body: SafeArea(
+           child: Padding(
+          padding: EdgeInsets.all(16.0),
+           )
+        )
+    );
   }
 }
