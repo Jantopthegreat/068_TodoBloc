359bd82394032e6c6b10f8ac37e25831100d4a92
Jantopthegreat <fauzangriya@gmail.com>
2025-05-08 22:56:07 +0700

    membuat validasi untuk form

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -76,6 +76,12 @@ class TodoPage extends StatelessWidget {
                           labelText: 'Todo',
                           border: OutlineInputBorder(),
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a todo';
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ],
